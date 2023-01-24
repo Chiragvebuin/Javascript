77ff3cf134467f3ae8c741f4fa503b2dd9d54c0b
Chiragvebuin <chiragmenariya@vebuin.com>
2023-01-24 16:21:07 +0530

    other files

--- task-3.js
+++ task-3.js
@@ -1,13 +1,13 @@
 const arr1=[17,21,23];
 const arr2=[12, 5, -5, 0, 4];
 
-console.log(`${arr2[0]}ºC in ${[0+1]} days...${arr2[1]}ºC in ${[0+2]} days...`);
+console.log(`${arr2[0]}°C in ${[0+1]} days...${arr2[1]}°C in ${[0+2]} days...`);
 const printForecast=function(arr)
 {
    let res="";
    for(i=0;i<arr.length;i++)
    {
-     res+=`${arr[i]}ºC in ${[i+1]} days...`;
+     res+=`${arr[i]}°C in ${[i+1]} days...`;
    }
    return res;
 }
