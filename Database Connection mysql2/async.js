b01fe14efe5e2b457ab5885b0f95d6a6d9621aa1
Chiragvebuin <Chiragmenariya@vebuin.com>
2023-02-10 18:43:02 +0530

    awdawda

--- Database Connection mysql2/async.js
+++ Database Connection mysql2/async.js
@@ -6,15 +6,15 @@ const app = express();
 app.use(bodyParser.json());
 const port = 4000;
 
-app.get('/users', async(req, res) => {
-    const query = "SELECT * From user";
-    connection.query(query, function (err, rows) {
-      if (err) {
-        console.log("An error occured with the quyery");
-        return;
-      }
-      res.send(rows);
-    });
-})
+app.get('/users', async (req, res) => {
+  try {
+    const query = 'SELECT * From user';
+    const [rows] = await connection.query(query);
+    res.send(rows);
+  } catch (err) {
+    console.log('An error occured with the query');
+    res.status(500).send({ message: 'An error occured with the query' });
+  }
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
---
+++ Namaste_JavaScript/Errors/referenceError.js
@@ -0,0 +1,5 @@
+console.log(a);
+console.log(b);
+console.log(c);
+let a = 10;
+var b = 100;
