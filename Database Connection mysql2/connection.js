c237e450bf8e3e84f71400319c5ddfa067de8ad6
Chiragvebuin <Chiragmenariya@vebuin.com>
2023-02-13 18:01:10 +0530

    UPGRADE

--- Database Connection mysql2/connection.js
+++ Database Connection mysql2/connection.js
@@ -1,9 +1,9 @@
 //creating a connection
 import mysql from 'mysql2';
-export const connection = mysql.createPool({
+export const connection = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "", // empty pass if local
-    database: "data",
-    port: 3307
+    database: "creation",
+    port: 3306
 });
