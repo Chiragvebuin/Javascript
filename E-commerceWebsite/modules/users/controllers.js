f172c6cf1eb3b6d751f69bdb5188fe4de0fe47b8
Chiragvebuin <rajesh@vebuin.com>
2023-02-20 18:35:27 +0530

    Changes

--- E-commerceWebsite/modules/users/controllers.js
+++ E-commerceWebsite/modules/users/controllers.js
@@ -3,8 +3,8 @@ import { selectUsers } from "./services.js";
 export const getUsersController = async (req, res, next) => {
   try {
     const users = await selectUsers();
-    console.log(users);
-    return res.send(users);
+    console.log("==================",users[0]);
+    return res.send(users[0]);
   } catch (error) {
      res.status(500).end("ISE");
 
